60b34ad3e29c89d9f700bf5f889fbc5a6f894a71
martsot <93199076+martsot@users.noreply.github.com>
2021-10-26 06:29:12 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const grid = new Muuri('.grid', {
+	layout: {
+		rounding: false
+	}
+});
+
+window.addEventListener('load', () => {
+	grid.refreshItems().layout();
+	document.getElementById('grid').classList.add('imagenes-cargadas');
+
+
+	// Agregamos listener para las imagenes
+	const overlay = document.getElementById('overlay');
+	document.querySelectorAll('.grid .item img').forEach((elemento) => {
+		elemento.addEventListener('click', () => {
+			const ruta = elemento.getAttribute('src');
+			const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+			overlay.classList.add('activo');
+			document.querySelector('#overlay img').src = ruta;
+			document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+		});
+	});
+
+	
+});// JavaScript Document
